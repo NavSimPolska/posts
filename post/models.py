4adc9b1d178080d65cf6477f0531c65115dfc3dc
NavSimPolska <jacek.pietraszkiewicz@navsim.pl>
2021-09-11 16:36:13 +0200

    Koniec

--- post/models.py
+++ post/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
-
 class Posts(models.Model):
     title   =  models.CharField(max_length = 256, blank=False)
     content =  models.TextField(blank=False)
     created =  models.DateTimeField(auto_now_add = True)
     modified = models.DateTimeField(auto_now=True)
+    author_id = models.IntegerField(null=False)
 
-class author(models.Model):
+    def __str__(self):
+        return f"id:{self.id}, title={self.title}, content={self.content}, created={self.created}"
+
+    # def save(self):
+    #     self.save()
+    #     return
+
+
+class Author(models.Model):
     nick = models.CharField(max_length=50, blank=False, null=False, unique=True)
     email = models.EmailField(max_length=254, blank=False)
+
+    def __str__(self):
+        return f"id:{self.id}, nick={self.nick}, email={self.email}"
