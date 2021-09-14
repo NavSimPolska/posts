c359c8f129692a9bf826c0a5b51e055f2559c82b
NavSimPolska <jacek.pietraszkiewicz@navsim.pl>
2021-09-14 14:39:38 +0200

    Ciag dalszy projektu...

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from post.models import Posts, Author
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'author_id']
+    list_filter = ['author_id']
+    search_fields = ['title']
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nick']
+
 # Register your models here.
-admin.site.register(Posts)
-admin.site.register(Author)
+admin.site.register(Posts, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
--- post/views.py
+++ post/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
+from post.models import Posts, Author
 
-# Create your views here.
+def post_list(request):
+   posty = Posts.objects.all()
+   t = loader.get_template("post/main.html")
+   c = {"title": "Lista postów", "posty": posty}
+   return HttpResponse(t.render(c))
+
+def post_details(request, p):
+   post = Posts.objects.get(id = p)
+   t = loader.get_template("post/main.html")
+   c = {"title": "Szczegóły posta", "post": post}
+   return HttpResponse(t.render(c))
+
+def author_list(request):
+   autorzy = Author.objects.all()
+   t = loader.get_template("post/author_list.html")
+   c = {"title": "Lista autorów", 'autorzy': autorzy}
+   return HttpResponse(t.render(c))
+
+def author_details(request, a):
+   auth = Author.objects.get(id = a)
+   t = loader.get_template("post/author_details.html")
+   c = {"title": "O autorze", "auth": auth}
+   return HttpResponse(t.render(c))
